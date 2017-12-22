2b3bc9048373bd515feabbf9db8971c7d4edcb95
Administrator <Administrator@DJA-PC>
2017-12-22 11:44:50 +0900

    .

--- exam/src/p20/Excute.java
+++ exam/src/p20/Excute.java
@@ -1,28 +1,70 @@
 package p20;
 
 import java.util.LinkedHashMap;
+import java.util.Scanner;
 
 public class Excute {
-	public static void main(String[] args) {
-		Service s = new Service();
-		LinkedHashMap<String, Object> hm = new LinkedHashMap<String, Object>();
-		hm.put("cidesc","네트워크보안반");
-		hm.put("cino",3);
-		int result = s.updateClassInfo(hm);
-		if(result==1) {
-			System.out.println("업데이트 잘됬음!");
-		}
-		
-		hm=new LinkedHashMap<String,Object>();
-		hm.put("cino",11);
-		int  result1 =s.deleteClassInfo(hm);
-		if(result1==1) {
-			System.out.println("삭제 잘됬음!");
-		}
+	Service s= new Service();
+	Scanner scan = new Scanner(System.in);
 	
- 
-	
-	
+	public int update(LinkedHashMap<String,Object> hm,int option) {
+		hm = new LinkedHashMap<String,Object>();
+		System.out.println("변경 할 클래스 넘버를 입력해주세요");
+		scan.nextLine().trim();
+		hm.put("cino",scan.nextLine().trim());
+		System.out.println("변경 할 클래스 정보를 입력해주세요");
+		scan.nextLine().trim();
+		hm.put("cidesc",scan.nextLine().trim());
+		int result = s.makeSql(option,hm);
+		return result;
 	}
-
+	public int insert(LinkedHashMap<String,Object> hm,int option) {
+		hm = new LinkedHashMap<String,Object>();
+		System.out.println("추가할 클래스 이름을 입력해주세요");
+		scan.nextLine().trim();
+		hm.put("ciname",scan.nextLine().trim());
+		System.out.println("클래스 정보를 입력해주세요");
+		scan.nextLine().trim();
+		hm.put("cidesc",scan.nextLine().trim());
+		int result = s.makeSql(option,hm);
+		return result;
+	}
+	public int delete(LinkedHashMap<String,Object> hm,int option) {
+		hm = new LinkedHashMap<String,Object>();
+		System.out.println("추가할 클래스 번호를 입력해주세요");
+		scan.nextLine().trim();
+		hm.put("cino",scan.nextLine().trim());
+		int result = s.makeSql(option,hm);
+		return result;
+	}
+	public int select(LinkedHashMap<String,Object> hm,int option) {
+		hm = new LinkedHashMap<String,Object>();
+		System.out.println("출력 할 클래스 번호를 입력해주세요");
+		scan.nextLine().trim();
+		hm.put("cino",scan.nextLine().trim());
+		return s.makeSql(option,hm);
+	}
+	public static void main(String[] args) {
+		LinkedHashMap<String,Object> hm = new LinkedHashMap<String,Object>();
+		
+		Excute ex = new Excute();
+		int result =0;
+		
+		System.out.println("1.목록보기 2.삽입 3.삭제 4.편집 ");
+		int option = ex.scan.nextInt();
+		if(option==1) {
+			ex.select(hm,option);
+		}else if(option ==2) {
+			result=ex.insert(hm,option);
+		}else if(option ==3) {
+			result=ex.delete(hm,option);
+		}else if(option ==4) {
+			result=ex.update(hm,option);
+		}else {	
+			System.out.println("잘못입력");
+		}
+		if(result==1 || result==4) {
+			System.out.println("실행되었습니다");
+		}
+	}
 }
